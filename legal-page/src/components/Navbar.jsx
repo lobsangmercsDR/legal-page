211a6a1b13d203df79b044c206a2f1eaec572131
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-07-07 15:35:05 -0300

    feat: whatsapp

--- legal-page/src/components/Navbar.jsx
+++ legal-page/src/components/Navbar.jsx
@@ -15,23 +15,29 @@ function Navbar() {
       <div className='contact-content'>
         <div style={{display: 'flex', alignItems: 'center', gap: '4px'}}>
           <MailOutlinedIcon fontSize="small" />
+          <a href="mailto:contato@henriqueribeiro.adv.br">
           <span>
             contato@henriqueribeiro.adv.br
           </span>
+          </a>
         </div>
         
         <div style={{display: 'flex', alignItems: 'center', gap: '4px'}}>
           <PhoneInTalkOutlinedIcon fontSize="small" />
+          <a href="tel:31991111501">
           <span>
             31 99111-1501
            </span>
+          </a>
         </div>
 
         <div style={{display: 'flex', alignItems: 'center', gap: '4px'}}>
           <QuestionAnswerOutlinedIcon fontSize="small" />
+          <a href="https://wa.me//553196438846?text=Tenho%20interesse%20em%20contactar%20um%20advogado%20especialista!" target="_blank" rel="noreferrer">
           <span>
             WhatsApp
           </span>
+          </a>
         </div>
       </div>
     </div>
