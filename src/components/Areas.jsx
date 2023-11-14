ff059e94474e31ba4deb42017fe16e44f14f45ae
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-11-14 15:26:08 -0300

    fix text css

--- src/components/Areas.jsx
+++ src/components/Areas.jsx
@@ -10,9 +10,9 @@ function Areas() {
       <p className='areas-title animate__animated animate__fadeIn'>
         Áreas de Atuação
       </p>
-      <div className='service-tag areas-title animate__animated animate__fadeIn' />
-      <span className='services-span areas-title animate__animated animate__fadeIn'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      {/* <div className='service-tag areas-title animate__animated animate__fadeIn' /> */}
+      <span className='aaa-span areas-title animate__animated animate__fadeIn'>
+        Em nosso escritório a expertise se entrelaça com a dedicação, oferecendo soluções jurídicas abrangentes em três pilares fundamentais: direito civil, laboral e de família. Em cada área de atuação, comprometemo-nos a ser a voz que traduz desafios em resoluções, seja na complexidade das relações familiares, nas nuances do universo laboral ou nos intricados cenários do direito civil. Aqui, a nossa missão é trilhar ao seu lado, construindo alicerces sólidos para sua vida e negócios. 
       </span>
       <span className='areas-span '>Processos Trabalhistas</span>
       <div className='area-um ' />
